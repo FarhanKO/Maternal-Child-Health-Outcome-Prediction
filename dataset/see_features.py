d087dc7d10bbd537aece9653169c96ceace8367c
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-07-26 09:26:25 +0600

    Create see_features.py

---
+++ dataset/see_features.py
@@ -0,0 +1,7 @@
+"""
+Create a extracted_features.txt in the same directory of this code and run it.
+It will get all the features from the csv and write them in the txt file.
+"""
+from pathlib import Path
+import csv
+
